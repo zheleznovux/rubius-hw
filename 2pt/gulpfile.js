621ff3f6f38908e51d4533cdc53a01406b5bd133
zheleznovux <66991410+zheleznovux@users.noreply.github.com>
2021-03-09 16:35:35 +0700

    gulp init

---
+++ 2pt/gulpfile.js
@@ -0,0 +1,64 @@
+const { src, dest, task, watch, series, parallel } = require('gulp');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const browserSync = require('browser-sync').create();
+const sourcemaps = require('gulp-sourcemaps');
+const del = require('del');
+
+
+function buildSass() {
+    return src('src/scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .on('error', sass.logError)
+        .pipe(postcss([
+            autoprefixer({
+                grid: true,
+                overrideBrowserslist: ['last 2 versions'],
+            }),
+             cssnano()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('dist/css'))
+        .pipe(dest('src/css'))
+        .pipe(browserSync.stream());
+}
+
+function browsersync()
+{
+    browserSync.init({
+        server: 'src/',
+        notify: false,
+    });
+}
+
+function html() {
+    return  src('src/**/*.html')
+            .pipe(dest('dist/'))
+            .pipe(browserSync.stream());
+}
+
+function liveServer()
+{
+    watch('src/scss/**/*.scss', buildSass);
+    watch('src/**/*.html',html);
+}
+
+function copy()
+{
+    return src(['src/images/**/*.*', 'src/css/**/*.scc'], {
+        base: 'src/',
+    }).pipe(dest('dist'));
+}
+
+function cleanDist() 
+{
+    return del('dist/**/*', {force: true});
+}
+
+exports.liveServer = liveServer;
+exports.buildSass = buildSass;
+
+exports.build = series(cleanDist,buildSass, html, copy);
+exports.default = series(buildSass, parallel(browsersync, liveServer));
